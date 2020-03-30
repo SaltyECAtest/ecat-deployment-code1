b5a8e95b79614356927420125b03e5aadaa5c13c
KingSaltyTest <57724228+KingSaltyTest@users.noreply.github.com>
2020-03-30 15:26:23 +0100

    Update project.tf

--- gcp/project.tf
+++ gcp/project.tf
@@ -24,42 +24,23 @@ output "project_id" {
  value = google_project.project.project_id
 }
 
-resource "null_resource" "before1" {
+resource "null_resource" "before" {
 }
 
-resource "null_resource" "delay1" {
+resource "null_resource" "delay" {
   provisioner "local-exec" {
     command = "sleep 300"
   }
   triggers = {
-    "before1" = "${null_resource.before1.id}"
+    "before" = "${null_resource.before.id}"
   }
-}
-
-resource "null_resource" "after1" {
-  depends_on = ["null_resource.delay1"]
 }
 
 resource "google_compute_network" "vpc_network" {
+  depends_on = ["null_resource.delay"]
   name = "salty-test"
   auto_create_subnetworks = true
   project = google_project.project.project_id
-}
-
-resource "null_resource" "before2" {
-}
-
-resource "null_resource" "delay2" {
-  provisioner "local-exec" {
-    command = "sleep 300"
-  }
-  triggers = {
-    "before2" = "${null_resource.before2.id}"
-  }
-}
-
-resource "null_resource" "after" {
-  depends_on = ["null_resource.delay2"]
 }
 
 resource "google_compute_instance" "vm_instance" {
